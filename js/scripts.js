24c14eb329a5a7fb196e8538069256d05595298a
Gray Hildreth <grayhildreth@gmail.com>
2017-03-29 15:43:56 -0700

    added toggle functionality to service icons using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,27 @@
 
   navButton.onclick = toggleMobileMenu;
 }());
+
+$(document).ready(function(){
+  $(".conceptualization").click(function(){
+    $("#conceptualization-copy").toggle(1000);
+  });
+});
+
+$(document).ready(function(){
+  $(".field").click(function(){
+    $("#field-copy").toggle(1000);
+  });
+});
+
+$(document).ready(function(){
+  $(".post").click(function(){
+    $("#post-copy").toggle(1000);
+  });
+});
+
+$(document).ready(function(){
+  $(".distribution").click(function(){
+    $("#distribution-copy").toggle(1000);
+  });
+});
